ac022ce8792914bcd2087d0007646f3a051a1914
Harsh <harshkhavale1102@gmail.com>
2023-08-01 11:13:32 +0530

    lit changes here & there

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -12,7 +12,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const { _id } = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
-
+  const currentUser = useSelector((state) => state.user._id)
   const { palette } = useTheme();
   const primaryLight = palette.primary.light;
   const primaryDark = palette.primary.dark;
@@ -64,16 +64,20 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      
+      {
+        friendId !== currentUser &&( <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>)
+      }
+     
     </FlexBetween>
   );
 };
--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -11,6 +11,7 @@ import UserWidget from "../widgets/UserWidget";
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
+  const currentUser = useSelector((state)=>state.user._id)
   const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
@@ -30,16 +31,17 @@ const ProfilePage = () => {
   if (!user) return null;
 
   return (
-    <Box>
+    <Box >
+    
       <Navbar />
-      <Box
+      <Box 
         width="100%"
         padding="2rem 6%"
         display={isNonMobileScreens ? "flex" : "block"}
         gap="2rem"
         justifyContent="center"
       >
-        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+        <Box mt={isNonMobileScreens ? "" : "2rem"} flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={userId} picturePath={user.picturePath} />
           <Box m="2rem 0" />
           <FriendListWidget userId={userId} />
@@ -47,8 +49,10 @@ const ProfilePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
-        >
-         
+        >  {
+          currentUser === userId && ( <MyPostWidget picturePath={user.picturePath}/>)
+        }
+        
           <Box m="2rem 0" />
           <PostsWidget userId={userId} isProfile />
         </Box>
